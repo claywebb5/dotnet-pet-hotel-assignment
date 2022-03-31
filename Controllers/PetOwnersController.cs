a447f86de38da92da55c71961738dff370ce5407
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-03-31 14:55:12 -0500

    post route created and tested in postman!

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -20,7 +20,17 @@ namespace pet_hotel.Controllers
         // occur when the route is missing in this controller
         [HttpGet]
         public IEnumerable<PetOwner> GetPets() {
-            return new List<PetOwner>();
+            // return new List<PetOwner>();
+            return _context.PetOwners;
         }
+
+        [HttpPost]
+        public IActionResult Create(PetOwner petOwner) {
+                _context.Add(petOwner);
+                _context.SaveChanges();
+
+             return CreatedAtAction(nameof(Create), new { id = petOwner.id }, petOwner);
+        }
+        
     }
 }
