d94ee01e71a6cd9e5c7a30e0a337b76a9e5cd5f4
Clay Webb <clay_webb@outlook.com>
2022-04-01 10:08:28 -0500

    Created database with tables

---
+++ Migrations/20220401150459_CreatePetOwnersTable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace dotnet_bakery.Migrations
+{
+    public partial class CreatePetOwnersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    checkIn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    type = table.Column<int>(type: "integer", nullable: false),
+                    ownedById = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Pets_PetOwners_ownedById",
+                        column: x => x.ownedById,
+                        principalTable: "PetOwners",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_ownedById",
+                table: "Pets",
+                column: "ownedById");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+        }
+    }
+}
